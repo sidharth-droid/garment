90eca1c8052b90578e6d82ddf82ffedf74faeae9
sidharth-droid <sidharth2432004@gmail.com>
2025-05-23 16:07:29 +0530

    Commit

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,8 +1,8 @@
 // let API_URI = 'https://garment002.pythonanywhere.com/';
 // let API_URI = 'http://127.0.0.1:8000';
-let API_URI = 'Retailplush001.pythonanywhere.com';
+let API_URI = 'https://retailplush001.pythonanywhere.com/';
 if (process.env.NODE_ENV === 'development') {
   // API_URI = 'http://127.0.0.1:8000';
-  API_URI = 'Retailplush001.pythonanywhere.com';
+  API_URI = 'https://retailplush001.pythonanywhere.com/';
 }
 export const API_BASE_URL = API_URI;
